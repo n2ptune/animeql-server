4ea7d6688647f11cd60a7cb801a07b8f430e9e8b
n2ptune <n2ptune@outlook.com>
2020-02-25 11:43:36 +0900

    changed: logic of fetch relation animes

--- graphql/db.js
+++ graphql/db.js
@@ -94,12 +94,38 @@ export const getRelationAnimeByID = async id => {
     relatedIDs.push(anime.attributes.slug)
   })
 
-  const { data: relatedAnimes } = await axios.get(`${APP_URL}`, {
-    params: {
-      'filter[categories]': relatedIDs.join(','),
-      sort: '-updatedAt'
-    }
+  const axiosWrapper = []
+
+  // 모든 카테고리에 대한 Promise 객체를 만듬
+  relatedIDs.map(id => {
+    axiosWrapper.push(
+      axios.get(`${APP_URL}`, {
+        params: {
+          'filter[categories]': id,
+          sort: '-createdAt',
+          limit: 3
+        }
+      })
+    )
   })
 
-  return relatedAnimes.data
+  // 모든 Promise 객체 통신
+  const [...data] = await Promise.all(axiosWrapper)
+
+  // 가져온 데이터에서 애니메이션 관련 데이터만 가져와서 평평하게 만듬
+  const relatedAnimes = data.map(el => el.data.data).flat()
+
+  // 아이디 값에서 중복 제거
+  const allIDs = relatedAnimes.map(e => parseInt(e.id))
+  const removedOverlap = Array.from(new Set(allIDs))
+
+  const result = []
+
+  // 중복 제거한 아이디 값에서 해당 아이디를 가진 객체만 가져와서
+  // 결과 배열에 담음
+  removedOverlap.forEach(id => {
+    result.push(relatedAnimes.find(anime => parseInt(anime.id) === id))
+  })
+
+  return result
 }
